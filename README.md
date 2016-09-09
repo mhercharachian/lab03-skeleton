f54c6b8d66204dc4b880b8a5c5bace651023758a
P Campbell <pcampbell.edu@gmail.com>
2016-09-09 17:04:12 -0400

    initial

---
+++ README.md
@@ -0,0 +1,16 @@
+# week03-lab-skeleton	
+# Activity Lab
+code skeleton and instructions for lab week 3
+using state Bundle and Shared Preferences
+
+
+*PDF lab instructions are in the assets folder*
+[pdf here](assets/lab03-activity-logging-state-prefs.pdf)
+
+Use of logcat to watch the logging
+
+
+When you extract this code you may need to change the target SDK
+
+eclipse: Properties->Android->5.1.1 (select)
+
---
+++ app/src/main/java/course/labs/activitylab/ActivityOne.java
@@ -0,0 +1,72 @@
+package course.labs.activitylab;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+public class ActivityOne extends Activity {
+
+		// string for logcat documentation
+		private final static String TAG = "Lab-ActivityOne";
+
+	
+		// lifecycle counts	
+		//TODO:
+		//Create 7 counter variables, each corresponding to a different one of the lifecycle callback methods.
+		// You will need to increment these variables' values when their corresponding lifecycle methods get called.  
+		
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.activity_one);
+			
+			//Log cat print out
+			Log.i(TAG, "onCreate called");
+			
+			//TODO:
+			//update the appropriate count variable
+			//update the view
+		}
+
+		@Override
+		public boolean onCreateOptionsMenu(Menu menu) {
+			// Inflate the menu; this adds items to the action bar if it is present.
+			getMenuInflater().inflate(R.menu.activity_one, menu);
+			return true;
+		}
+		
+		// lifecycle callback overrides
+		
+		@Override
+		public void onStart(){
+			super.onStart();
+			
+			//Log cat print out
+			Log.i(TAG, "onStart called");
+			
+			//TODO:
+			//update the appropriate count variable
+			//update the view 
+		}
+
+	    // TODO: implement 5 missing lifecycle callback methods
+
+		@Override
+		public void onSaveInstanceState(Bundle savedInstanceState){
+			//TODO:
+			// save state information with a collection of key-value pairs
+			// save all  count variables
+		}
+		
+		public void launchActivityTwo(View view) {
+			//TODO:
+			// This function launches Activity Two. 
+			// Hint: use Context’s startActivity() method.
+		}
+		
+
+}
