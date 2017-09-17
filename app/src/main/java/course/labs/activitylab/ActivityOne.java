c9e110ba9479fc1547031f4a222014de096cc787
Mher <charachianmher@live.com>
2017-09-17 17:32:25 -0400

    latest commit

--- app/src/main/java/course/labs/activitylab/ActivityOne.java
+++ app/src/main/java/course/labs/activitylab/ActivityOne.java
@@ -8,6 +8,8 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class ActivityOne extends Activity {
 
 		// string for logcat documentation
@@ -15,17 +17,30 @@ public class ActivityOne extends Activity {
 
 		// lifecycle counts
 		//TODO: Create 7 counter variables, each corresponding to a different one of the lifecycle callback methods.
+		int createCount = 0;
+		int startCount = 0;
+		int resumeCount = 0;
+		int pauseCount = 0;
+		int stopCount = 0;
+		int restartCount = 0;
+		int destroyCount = 0;
+		TextView create, start, resume, pause;
+
 		//TODO:  increment the variables' values when their corresponding lifecycle methods get called.
 		
 		@Override
 		protected void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.activity_one);
-			
+
 			//Log cat print out
 			Log.i(TAG, "onCreate called");
 			
 			//TODO: update the appropriate count variable & update the view
+			createCount++;
+			create = (TextView) findViewById(R.id.create);
+			String text = getString(R.string.onCreate);
+			create.setText(text + " " + createCount);
 		}
 
 		@Override
@@ -40,14 +55,57 @@ public class ActivityOne extends Activity {
 		@Override
 		public void onStart(){
 			super.onStart();
-			
 			//Log cat print out
 			Log.i(TAG, "onStart called");
 			
 			//TODO:  update the appropriate count variable & update the view
+			startCount++;
+			start = (TextView) findViewById(R.id.start);
+			String text = getString(R.string.onStart);
+			start.setText(text + " " + startCount);
 		}
 
-	    // TODO: implement 5 missing lifecycle callback methods
+		@Override
+		public void onResume(){
+			super.onResume();
+			//Log cat print out
+			Log.i(TAG, "onResume called");
+
+			//TODO:  update the appropriate count variable & update the view
+			resumeCount++;
+			resume = (TextView) findViewById(R.id.resume);
+			String text = getString(R.string.onResume);
+			resume.setText(text + " " + restartCount);
+		}
+
+		@Override
+		public void onPause(){
+			super.onPause();
+			//Log cat print out
+			Log.i(TAG, "onPause called");
+
+			//TODO:  update the appropriate count variable & update the view
+			pauseCount++;
+			pause = (TextView) findViewById(R.id.pause);
+			String text = getString(R.string.onPause);
+			pause.setText(text + " " + pauseCount);
+		}
+
+		@Override
+		public void onStop(){
+		super.onStop();
+		//Log cat print out
+		Log.i(TAG, "onPause called");
+
+		//TODO:  update the appropriate count variable & update the view
+		pauseCount++;
+		pause = (TextView) findViewById(R.id.pause);
+		String text = getString(R.string.onPause);
+		pause.setText(text + " " + pauseCount);
+	}
+
+
+	// TODO: implement 5 missing lifecycle callback methods
 
 		@Override
 		public void onSaveInstanceState(Bundle savedInstanceState){
